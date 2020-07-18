e129b72969706e0c53aa52b3a0dfaf47053a6db3
Astig-1982 <astig.mandalian2014@gmail.com>
2020-07-18 10:19:32 +0000

    Created the form and display message

--- run.py
+++ run.py
@@ -1,24 +1,41 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html") 
 
 
 @app.route("/about")
 def about():
     data = []
-    with open("data/company.json", "r") as json.data:
-        data=json.load(json.data)
+    with open("data/company.json", "r") as json_data:
+        data=json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
 
 
-@app.route("/contact")
+@app.route("/about/<hero_name>")
+def about_hero(hero_name):
+    hero = {}
+    with open("data/company.json", "r") as json_data:
+        data=json.load(json_data)
+        for obj in data:
+          if obj["url"] == hero_name:
+            hero = obj
+    return render_template("hero.html", hero=hero)
+
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact Us")
 
 
